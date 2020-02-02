cdf34ef95ea4ed83b10ac4e95cb6280b5a39171a
colour10 <84299915@qq.com>
2020-02-02 11:14:48 +0800

    性能优化

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,1 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::prefix('v1')->namespace('Api')->name('api.')->group(function () {
-    Route::get('test', function () {
-        return 'test';
-    })->name('test');
-});
-
