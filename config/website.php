d2117c7247bc790dca2dca56c5282dfce07b54dc
colour10 <84299915@qq.com>
2020-02-02 12:49:03 +0800

    优化

--- config/website.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'title'       => '【刘宗阳博客】-一个优秀的天津PHP程序员个人技术博客',
-    'keywords'    => 'php程序员,程序员,php教程,web前端开发,mysql教程,linux学习,个人博客,技术博客,刘宗阳,刘宗阳博客,天津PHP,天津PHP程序员',
-    'description' => '刘宗阳博客是一个优秀的天津PHP程序员个人技术博客，博客记录了Linux学习与教程、PHP开发与框架、Web前端与学习、MySQL运维技巧、NoSQL数据库以及个人的经历和观点。',
-];
